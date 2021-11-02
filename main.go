0af6112780b487c9dd64d84f5e5234e27c02c83f
Naru Koshin <naru.koshin@outlook.jp>
2021-11-02 19:19:42 +0200

    added vendor folder to routes

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"text/template"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := make(map[string]interface{})
+		data["Site"] = map[string]string {
+			"Title": "Hello, world",
+		}
+		t := template.Must(template.ParseFiles("templates/home.blade.html"))
+		t.Execute(w, data)
+	})
+	router.HandleFunc("/assets/video/{name}", func(w http.ResponseWriter, r *http.Request) {
+		name := mux.Vars(r)["name"]
+		location := "assets/video/" + name
+		if _, err := os.Stat(location); !os.IsNotExist(err) {
+			http.ServeFile(w, r, location)
+		}
+	})
+	router.HandleFunc("/vendor/{name}", func(w http.ResponseWriter, r *http.Request) {
+		name := mux.Vars(r)["name"]
+		location := "vendor/" + name
+		if _, err := os.Stat(location); !os.IsNotExist(err) {
+			http.ServeFile(w, r, location)
+		} 
+	})
+	http.ListenAndServe(":8000", router)
+}
