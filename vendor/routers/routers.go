b587a82e36887806ba8eb4227c8e7ee4e4ccaaac
Naru Koshin <naru.koshin@outlook.jp>
2021-11-05 15:52:12 +0200

    created routers

---
+++ vendor/routers/routers.go
@@ -0,0 +1,19 @@
+package routers
+
+import (
+	"github.com/gorilla/mux"
+	"controller"
+)
+
+func Route() *mux.Router {
+	/*** Starting the Mux Router ***/
+	route := mux.NewRouter()
+
+	/*** Handling the routes ***/
+	route.HandleFunc("/", controller.HomeController)
+	route.HandleFunc("/login", controller.LoginController)
+	route.HandleFunc("/resources/{folder}/{file}", controller.ResourcesController)
+
+	/*** Returning mux.Router ***/
+	return route
+}
