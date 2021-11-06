96dbfeca1c704f573d13cc3aa716a3de86e28be6
Naru Koshin <naru.koshin@outlook.jp>
2021-11-06 15:24:59 +0200

    added gzip compression and added namespace to local imports

--- main.go
+++ main.go
@@ -3,15 +3,18 @@ package main
 import (
 	"net/http"
 
-	"logging"
-	"routers"
+	"narukoshin.me/logging"
+	"narukoshin.me/routers"
+
+	"github.com/nanmu42/gzip"
 )
 
 func main() {
 	logging.Init()
 
 	router := routers.Route()
-	err := http.ListenAndServe(":8000", router)
+
+	err := http.ListenAndServe(":8000", gzip.DefaultHandler().WrapHandler(router))
 	if err != nil {
 		logging.Save(err)
 	}
