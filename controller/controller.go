3983c32e310d907b8dafc46785c8c0ab6fdd72c8
Naru Koshin <naru.koshin@outlook.jp>
2021-11-06 19:48:40 +0200

    added gzip compression
    Idk why but the compression doesn't work on the server, is that because of CloudFlare?

--- controller/controller.go
+++ controller/controller.go
@@ -1,5 +1,6 @@
 package controller
 import (
+	"compress/gzip"
 	"text/template"
 	"net/http"
 	"os"
@@ -15,13 +16,20 @@ type Page struct {
 }
 
 func (v *View) Load(data interface{}) error {
+	// Setting headers to load the page instead of file downloading
+	v.Writer.Header().Add("Content-Type", "text/html;charset=utf-8")
+	v.Writer.Header().Add("Content-Encoding", "gzip")
+	// Preparing writer for gzip writer
+	gz := gzip.NewWriter(v.Writer)
+
 	fileLocation := "templates/" + v.View + ".blade.html"
+	defer gz.Close()
 	if _, err := os.Stat(fileLocation); !os.IsNotExist(err) {
 		t := template.Must(template.ParseFiles(fileLocation))
-		err = t.Execute(v.Writer, data)
+		err = t.Execute(gz, data)
 		return err
 	} else {
-		v.Writer.Write([]byte("Error: template file not found"))
+		gz.Write([]byte("Error: template file not found"))
 		return err
 	}
 }
